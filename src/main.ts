535c65f234d215640a60161e56f231b4d71aa06c
Clarice67 <156119549+Clarice67@users.noreply.github.com>
2024-01-31 17:43:29 -0300

    criação de rota

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,30 @@
 import express from "express"
-import {db, firestore} from '../bancos-de-dados/firebase'; 
+import { db, firestore } from '../bancos-de-dados/firebase';
+import e from "express";
 
 const app = express()
 
 
+app.get("/", (req, res) => {
+   res.send(" A minha primeira API")
+})
 
-app. listen(3000, function () {
+app.post("/usuario", async (req, res) => {
+   const usuario = req.body.nome
+
+   try {
+      const docRef = await firestore.addDoc(firestore.collection(db, 'usuarios'), {
+         nome: nome,
+      })
+      res.send(docRef.id)
+   } catch (error) { 
+     console.log(e)
+     res.status(500).send(e)
+   }
+})
+
+
+app.listen(3000, function () {
    console.log("Serviço rodando em http://localhost:3000");
 
 });
